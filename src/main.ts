97e9691bf8209e9a8a5ae0a82cc87cb626cdf9bc
Samuel <fanimokunbusayo@gmail.com>
2022-08-05 03:24:23 +0100

    preparing for dev to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe,ValidationError } from '@nestjs/common';
+import {ValidationException} from './filters/validation.exception';
+
+import {HttpExceptionFilter} from './filters/http.filter';
+import {FallbackExceptionFilter} from './filters/fallback.filter';
+import {ValidationFilter} from './filters/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+
+  app.useGlobalFilters(
+    new FallbackExceptionFilter(),
+    new HttpExceptionFilter(),
+    new ValidationFilter()
+);
+
+
+ /* * Apply validation for all inputs globally
+  */
+ app.useGlobalPipes(new ValidationPipe({
+  transform:true,
+  skipMissingProperties:true,
+  exceptionFactory: (errors: ValidationError[]) => {
+
+      const messages = errors.map(
+          error=> `${error.property} has wrong value ${error.value},
+          ${Object.values(error.constraints).join(', ')} `
+      );
+
+      return new ValidationException(messages);
+  }
+}));
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap();
